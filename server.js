3f010a704bc560199185d22069b2882e1e4e6644
Stanley Nguyen <hung.ngn.the@gmail.com>
2016-07-16 16:27:05 +0800

    fix repetition when server start

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var express = require("express"),
     app = express(),
     mongoose = require("mongoose"),
     bodyParser = require('body-parser');
-    
+
 mongoose.connect(process.env.DATABASE);
-    
+
 require("./bot/setup")();
 
 app.use(bodyParser.json());
 
 app.post('/' + process.env.TOKEN, function (req, res) {
-  require("./bot/bot").processUpdate(req.body);
+  //require("./bot/bot").processUpdate(req.body);
   res.sendStatus(200);
 });
 
